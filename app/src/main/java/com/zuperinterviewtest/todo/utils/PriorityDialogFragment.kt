f604c2b0ae43510a4b698c4cba45ce8afe27eef5
Sai Manidevi Bezawada <saimanidevi@gmail.com>
2022-02-09 10:37:16 +0530

    refactor: Remove priority picker dialog layout

--- app/src/main/java/com/zuperinterviewtest/todo/utils/PriorityDialogFragment.kt
+++ app/src/main/java/com/zuperinterviewtest/todo/utils/PriorityDialogFragment.kt
@@ -33,8 +33,6 @@ class PriorityDialogFragment : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            // Get the layout inflater
-            val inflater = requireActivity().layoutInflater
             // Inflate and set the layout for the dialog
             // Pass null as the parent view because its going in the dialog layout
             val priorityList = listOf("Low", "Medium", "High")
@@ -53,38 +51,6 @@ class PriorityDialogFragment : DialogFragment() {
                 listener.onTodoPriorityClick(selectedPriority)
                 dialogInterface.dismiss()
             }
-            // TODO: Check binding or ensure a better way to handle priority clicks
-            //builder.setView(inflater.inflate(R.layout.dialog_priority_picker, null))
-            // Initialize binding variable that references [DialogPriorityPickerBinding] and
-            // inflates the corresponding layout
-            //val binding: DialogPriorityPickerBinding = DialogPriorityPickerBinding.inflate(
-            //  LayoutInflater.from(activity)
-            //)
-            //binding.lifecycleOwner = activity
-            //builder.setView(binding.root)
-
-            /*// Handle Priority Low click
-            binding.cardPriorityLow.setOnClickListener {
-                DialogInterface.OnClickListener { dialogInterface, _ ->
-                    listener.onPriorityLowClick()
-                    dialogInterface.dismiss()
-                }
-            }
-            // Handle Priority Medium click
-            binding.cardPriorityMedium.setOnClickListener {
-                DialogInterface.OnClickListener { dialogInterface, _ ->
-                    listener.onPriorityMediumClick()
-                    dialogInterface.dismiss()
-                }
-            }
-            // Handle Priority High click
-            binding.cardPriorityHigh.setOnClickListener {
-                DialogInterface.OnClickListener { dialogInterface, _ ->
-                    listener.onPriorityHighClick()
-                    dialogInterface.dismiss()
-                }
-            }*/
-
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
